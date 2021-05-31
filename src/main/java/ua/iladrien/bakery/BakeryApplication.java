d311847bca7baf1e817d1e6eaad51eb3412cf76b
iladrien-hub <sliptchenko71@gmail.com>
2021-06-01 01:04:39 +0300

    Security start

--- src/main/java/ua/iladrien/bakery/BakeryApplication.java
+++ src/main/java/ua/iladrien/bakery/BakeryApplication.java
@@ -2,8 +2,9 @@ package ua.iladrien.bakery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class BakeryApplication {
 
 	public static void main(String[] args) {
